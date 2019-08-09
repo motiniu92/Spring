8626d32371ed6ad0625ce284c41a699075369f80
motiniu92 <43033677+motiniu92@users.noreply.github.com>
2019-08-09 20:24:18 +0600

    evi

---
+++ crudespring/src/main/java/com/example/crudespring/CrudespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudespringApplication.class, args);
+	}
+
+}
